c2fbdc4f0de9a5063240b9e8f3fc07b22f0f3957
= <=>
2018-08-30 18:17:48 +0200

    push Message.ts

---
+++ src/app/message-board/Message.ts
@@ -0,0 +1,12 @@
+export class Message {
+  title: String;
+  content: String;
+  dateCreated: String;
+
+
+  constructor(title: String, content: String, dateCreated: String) {
+    this.title = title;
+    this.content = content;
+    this.dateCreated = dateCreated;
+  }
+}
