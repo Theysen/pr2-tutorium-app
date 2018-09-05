c594218a6588b5570e8939562b3e0d13276e313e
Theysen <lukastheisen@gmx.de>
2018-09-05 17:19:40 +0200

    no idea what happened there

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnChanges, OnInit} from '@angular/core';
+import {LoginService} from "../login.service";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
 
-  constructor() { }
+  public loginUser() {
+    this.loginService.loginUser();
+  }
+
+  public getLoginStatus() {
+    this.loginService.loggedIn();
+  }
+
+  constructor(private loginService: LoginService) {
+  }
 
   ngOnInit() {
   }
+
 
 }
