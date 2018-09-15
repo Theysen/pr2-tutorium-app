24903cee7a2e3cc8deb70b3e51c2bf7a88a9aaf1
Patryk Preisner <1724532@stud.hs-mannheim.de>
2018-09-15 15:05:34 +0200

    rearanged whole project with more responsive design and more logical structure.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,14 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {MainBodyComponent} from "./main-body/main-body.component";
-import {CalendarComponent} from "./calendar/calendar.component";
-import {RegisterComponent} from "./register/register.component";
-import {ListComponent} from "./list/list.component";
 import {ComposeMessageComponent} from './compose-message/compose-message.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   {path: 'home', redirectTo: 'dashboard', pathMatch: 'full'},
-  {path: 'dashboard', component: MainBodyComponent},
-  {path: 'calendar', component: CalendarComponent},
-  {path: 'register', component: RegisterComponent},
-  {path: 'list', component: ListComponent},
-  {path: 'new', component: ComposeMessageComponent},
-  {path: 'd', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'newMessage', component: ComposeMessageComponent},
+
 ];
 
 @NgModule({
