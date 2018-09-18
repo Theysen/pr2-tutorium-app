83652332cb050b0e127190d765dfa2f35c326d7c
Theysen <lukastheisen@gmx.de>
2018-09-18 08:15:05 +0200

    added HammerJS for default mobile support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FlexLayoutModule} from '@angular/flex-layout';
 
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {ReactiveFormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AppComponent} from './app.component';
 import {CalendarComponent} from './components/body/dashboard/calendar/calendar.component';
 import {MessageBoardComponent} from './components/body/dashboard/message-board/message-board.component';
@@ -15,20 +14,20 @@ import {
   MatButtonModule,
   MatCardModule,
   MatCheckboxModule,
+  MatDatepickerModule,
   MatGridListModule,
   MatIconModule,
   MatInputModule,
   MatListModule,
   MatMenuModule,
+  MatNativeDateModule,
   MatPaginatorModule,
+  MatSelectModule,
   MatSidenavModule,
   MatSliderModule,
   MatSortModule,
   MatTableModule,
   MatToolbarModule,
-  MatSelectModule,
-  MatDatepickerModule,
-  MatNativeDateModule,
 } from '@angular/material';
 import {LayoutModule} from '@angular/cdk/layout';
 import {AppRoutingModule} from './app-routing.module';
@@ -36,7 +35,7 @@ import {MessageService} from './services/message.service';
 import {HttpClientModule} from '@angular/common/http';
 import {ComposeMessageComponent} from './components/body/compose-message/compose-message.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { DashboardComponent } from './components/body/dashboard/dashboard.component';
+import {DashboardComponent} from './components/body/dashboard/dashboard.component';
 
 
 @NgModule({
