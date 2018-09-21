f750f75ffa5dab6636640a7be34aceacb7def3c5
Theysen <lukastheisen@gmx.de>
2018-09-21 14:16:44 +0200

    implemented update in backend

--- pr2-backend/api/models/Date.js
+++ pr2-backend/api/models/Date.js
@@ -7,12 +7,15 @@ const dateSchema = mongoose.Schema({
   year: Number,
   bookedByGroup: String,
   subject: String,
-  bookedSlots: Number,
+  bookedSlots: {
+    type: Number,
+    max: 6
+  },
   possibleSlots: Number,
   startTime: String,
   endTime: String,
-  tutor: String
-  date: {type: Date, default: Date.now}
+  tutor: String,
+  date: { type: Date, default: Date.now }
 });
 
 module.exports = mongoose.model('Date', dateSchema);
